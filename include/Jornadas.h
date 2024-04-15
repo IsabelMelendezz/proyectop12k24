15d6e591609da9802e67bda416fee9fe50b022b2
BDG.Josue.Villagran <jvillagran@bdgsa.net>
2024-04-14 20:34:32 -0600

    Jornadas y Maestros

---
+++ include/Jornadas.h
@@ -0,0 +1,16 @@
+#ifndef JORNADAS_H
+#define JORNADAS_H
+
+#include<iostream>
+
+
+using std::string;
+
+class Jornadas
+{
+    public:
+		void menu();
+
+};
+
+#endif // JORNADAS_H
---
+++ include/Maestros.h
@@ -0,0 +1,17 @@
+#ifndef MAESTROS_H
+#define MAESTROS_H
+
+
+#include<iostream>
+
+
+using std::string;
+
+class Maestros
+{
+    public:
+		void menu();
+
+};
+
+#endif // MAESTROS_H
