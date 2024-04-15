f04965edfb6244c9abfde77c4c30844d76bf3bda
BDG.Josue.Villagran <jvillagran@bdgsa.net>
2024-04-14 20:20:47 -0600

    Asignaciones

---
+++ include/AsignacionAlumnos.h
@@ -0,0 +1,12 @@
+#ifndef ASIGNACIONALUMNOS_H
+#define ASIGNACIONALUMNOS_H
+
+
+class AsignacionAlumnos
+{
+    public:
+		void menu();
+
+};
+
+#endif // ASIGNACIONALUMNOS_H
---
+++ include/AsignacionFacultades.h
@@ -0,0 +1,16 @@
+#ifndef ASIGNACIONFACULTADES_H
+#define ASIGNACIONFACULTADES_H
+
+
+#include<iostream>
+
+
+using std::string;
+
+class AsignacionFacultades
+{
+    public:
+		void menu();
+
+};
+#endif // ASIGNACIONFACULTADES_H
